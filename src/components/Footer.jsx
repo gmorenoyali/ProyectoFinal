46e692efc2665ef049b8e175ec88a62bd479ee1c
GiiO <giiomy5@gmail.com>
2024-05-28 14:29:30 -0500

    Estilos para Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,8 @@
+
+/* 
 import React from 'react';
+
+import '../styles/Footer.css';
 
 const Footer = () => {
   return (
@@ -10,6 +14,33 @@ const Footer = () => {
         <li><a href="https://www.tiktok.com">TikTok</a></li>
         <li><a href="mailto:email@example.com">Correo</a></li>
       </ul>
+    </footer>
+  );
+};
+
+export default Footer;
+ */
+
+import React from 'react';
+import '../styles/Footer.css';
+
+import logo from '../assets/pascual1.png';  
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-logo-container">
+        <img src={logo} alt="Logo" className="footer-logo" />
+      </div>
+      <div className="footer-content">
+        <div>© 2024 Mi Empresa</div>
+        <div className="social-links">
+          <a href="https://facebook.com">Facebook</a>
+          <a href="https://instagram.com">Instagram</a>
+          <a href="https://tiktok.com">TikTok</a>
+          <a href="mailto:info@example.com">Correo</a>
+        </div>
+      </div>
     </footer>
   );
 };
