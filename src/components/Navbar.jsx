85041bac54d74474693d1b89826ce69a4ec9eaa9
GiiO <giiomy5@gmail.com>
2024-05-28 15:30:21 -0500

    Logo para Navbar
    Se modifico el componente Navbar.jsx para incluir logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
-
+/* import './Navbar.css'; */
 import '../styles/Navbar.css'
+
+import logo from '../assets/pascual1.png';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="logo">Logo</div>
-      <ul>
-        <li>Home</li>
-        <li>Servicios</li>
-        <li>Menu</li>
-        <li>Galeria</li>
-        <li>Contactanos</li>
+    <nav className="navbar">
+      <div className="navbar-logo-container">
+        <img src={logo} alt="Logo" className="navbar-logo" />
+      </div>
+      <ul className="navbar-menu">
+        <li className="navbar-item"><a href="#" className="navbar-link">Inicio</a></li>
+        <li className="navbar-item"><a href="#" className="navbar-link">Servicios</a></li>
+        <li className="navbar-item"><a href="#" className="navbar-link">Nosotros</a></li>
+        <li className="navbar-item"><a href="#" className="navbar-link">Contacto</a></li>
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
+
+
